055849aefaca71390a303abd83f1cf9e12be99fe
Ondrej Zdych <ondrej.zdych@firma.seznam.cz>
2020-04-23 20:17:09 +0200

    feat: finish chapter 7

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -29,6 +31,8 @@ public class Lox {
     // Indicate an error in the exit code.
     if (hadError)
       System.exit(65);
+    if (hadRuntimeError)
+      System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -52,11 +56,16 @@ public class Lox {
     if (hadError)
       return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where, String message) {
